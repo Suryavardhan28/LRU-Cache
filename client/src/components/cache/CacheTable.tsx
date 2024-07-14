2bfe438ef91987c9abc41e6ef296dbf24a69fd73
Suryavardhan28 <suryavardhan28@gmail.com>
2024-07-14 21:58:52 +0530

    Changes to Toggle Value Visibility

--- client/src/components/cache/CacheTable.tsx
+++ client/src/components/cache/CacheTable.tsx
@@ -1,15 +1,17 @@
 import {
     Grid,
     Paper,
+    Switch,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
     TableRow,
+    Tooltip,
     Typography,
 } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCache } from "../../redux/slices/cacheSlice";
 import { RootState } from "../../redux/store";
@@ -21,6 +23,8 @@ export default function CacheTable() {
         string,
         CacheItem
     >;
+
+    const [showValue, setShowValue] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -44,6 +48,21 @@ export default function CacheTable() {
             <Typography variant="h4" textAlign="center" mb={2}>
                 Live Cache Table
             </Typography>
+            <Tooltip title="Toggle value visibility" placement="right">
+                <Grid
+                    sx={{ width: "50%" }}
+                    container
+                    direction="row"
+                    justifyContent="flex-end"
+                    alignItems="center"
+                >
+                    <Typography>Show value</Typography>
+                    <Switch
+                        checked={showValue}
+                        onChange={() => setShowValue(!showValue)}
+                    />
+                </Grid>
+            </Tooltip>
             <TableContainer sx={{ width: "50%" }} component={Paper}>
                 <Table aria-label="cache-table">
                     <TableHead>
@@ -53,12 +72,13 @@ export default function CacheTable() {
                             }}
                         >
                             <TableCell>Key</TableCell>
+                            {showValue && <TableCell>Value</TableCell>}
                             <TableCell>Expiry</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {Object.entries(cache).map(
-                            ([key, { expiry }]: [string, CacheItem]) => (
+                            ([key, { expiry, value }]: [string, CacheItem]) => (
                                 <TableRow
                                     key={key}
                                     sx={{
@@ -70,6 +90,11 @@ export default function CacheTable() {
                                     <TableCell component="th" scope="row">
                                         <Typography>{key}</Typography>
                                     </TableCell>
+                                    {showValue && (
+                                        <TableCell align="left">
+                                            <Typography>{value}</Typography>
+                                        </TableCell>
+                                    )}
                                     <TableCell align="left">
                                         <Typography>{expiry}</Typography>
                                     </TableCell>
